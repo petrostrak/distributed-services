968da8d1bb12e56c19af0e8de0ef83f5e5f1f193
petrostrak <pit.trak@gmail.com>
2024-06-12 18:30:17 +0300

    ADD Log and Record struct with Append and Read methods

---
+++ LetsGo/internal/server/log.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	ErrOffsetNotFound = fmt.Errorf("offset not found")
+)
+
+type Record struct {
+	Value  []byte
+	Offset uint64
+}
+
+type Log struct {
+	mu      sync.RWMutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{
+		records: make([]Record, 0),
+	}
+}
+
+func (l *Log) Append(record Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	record.Offset = uint64(len(l.records))
+	l.records = append(l.records, record)
+
+	return record.Offset, nil
+}
+
+func (l *Log) Read(offset uint64) (*Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	if offset >= uint64(len(l.records)) {
+		return nil, ErrOffsetNotFound
+	}
+
+	return &l.records[offset], nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/petrostrak/proglog
+
+go 1.22.2
