7e4c7669bb4cf8acf26bf88dba89d34a409cfdac
petrostrak <pit.trak@gmail.com>
2024-06-12 19:02:09 +0300

    IMPLEMENT handleConsume

--- LetsGo/internal/server/http.go
+++ LetsGo/internal/server/http.go
@@ -66,4 +66,28 @@ func (s *HTTPServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *HTTPServer) handleConsume(w http.ResponseWriter, r *http.Request) {}
+func (s *HTTPServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	record, err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
