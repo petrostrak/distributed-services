f5c7aa3d63e8081846834651ce2dc036a0f1755e
petrostrak <pit.trak@gmail.com>
2024-06-13 13:35:03 +0300

    IMPLEMENT store.Append method

--- WriteALogPackage/internal/log/store.go
+++ WriteALogPackage/internal/log/store.go
@@ -23,8 +23,10 @@ const (
 // and read bytes to and from the file.
 type store struct {
 	*os.File
-	mu   sync.Mutex
-	buf  *bufio.Writer
+
+	mu  *sync.Mutex
+	buf *bufio.Writer
+
 	size uint64
 }
 
@@ -42,4 +44,33 @@ func newStore(f *os.File) (*store, error) {
 		size: size,
 		buf:  bufio.NewWriter(f),
 	}, nil
+}
+
+// Append persists the given bytes to the store.
+func (s store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// The segment will use this position when it creates an associated index
+	// entry for this record.
+	pos = s.size
+
+	// We write the length of the record so that, when we read the record,
+	// we know how many bytes to read.
+	err = binary.Write(s.buf, enc, uint64(len(p)))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// We write to the buffered writer instead of directly to the file to
+	// reduce the number of system calls and improve performance.
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	w += lenWidth
+	s.size += uint64(w)
+
+	return uint64(w), pos, nil
 }
