d81249415047dec75824dec374e216365999d0c8
petrostrak <pit.trak@gmail.com>
2024-06-18 17:30:03 +0300

    IMPLEMENT log.Truncate

--- internal/log/log.go
+++ internal/log/log.go
@@ -180,3 +180,24 @@ func (l *Log) HighestOffset() (uint64, error) {
 
 	return off - 1, nil
 }
+
+// Truncate removes all segments whose highest offset is lower than lowest.
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, s)
+	}
+
+	l.segments = segments
+
+	return nil
+}
